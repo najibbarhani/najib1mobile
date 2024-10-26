59dd3a92baa999e61bb8791e32153e30a96aa5f9
unknown <najibnasution6.com>
2024-10-27 00:46:23 +0700

    first commit

---
+++ lib/dashboard_page.dart
@@ -0,0 +1,39 @@
+// TUGAS MOBILE PROGRAMING FLUTTER NAJIB BARHANI
+import 'package:flutter/material.dart';
+
+class DashboardPage extends StatelessWidget {
+  final String username;
+
+  const DashboardPage({super.key, required this.username});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/background.jpg"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Text(
+            'SELAMAT DATANG DI TUGAS MOBILE PROGRAMMING, $username!',
+            style: const TextStyle(
+              fontSize: 24,
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              shadows: [
+                Shadow(
+                  blurRadius: 10.0,
+                  color: Colors.black,
+                  offset: Offset(2, 2),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
